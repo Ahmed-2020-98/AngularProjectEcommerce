2124518dea0a5d290ff69619e2cc5467cf04fb99
Ahmed-2020-98 <ahmedgamalmansour11@gmail.com>
2022-07-13 13:43:00 +0200

    three

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartsComponent } from './carts/carts/carts.component';
+import { AllProductsComponent } from './products/components/all-products/all-products.component';
+import { ProductsDetailsComponent } from './products/components/products-details/products-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'products',component:AllProductsComponent},
+  {path:'details',component:ProductsDetailsComponent},
+  {path:'cart', component:CartsComponent},
+  {path:'**', redirectTo:"products",pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/carts/carts.module.ts
+++ src/app/carts/carts.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CartsComponent } from './carts/carts.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CartsComponent
+  ],
   imports: [
     CommonModule
   ]
